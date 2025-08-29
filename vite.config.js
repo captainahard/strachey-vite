25751b436d3a6119cc9ca4755e944b80d3c4d9a0
captainahard <andrewhenryharding@gmail.com>
2025-08-29 15:49:41 -0400

    fix: correct Vite React SWC plugin and base path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@
 import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react-swc'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/strachey/',   // final path under glitchthink.com
+  base: '/strachey/',
 })
