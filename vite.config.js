1adabe3609fbcedb071f25ee9bfc55e30c93dee0
captainahard <andrewhenryharding@gmail.com>
2025-08-29 19:45:30 -0400

    fix: deploy app at root instead of /strachey

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react-swc'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/strachey/',
+  base: '/',   // root deployment
 })
